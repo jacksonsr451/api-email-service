b67a46abea576408646a7db52aa24d82d5ab8072
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-09-16 19:05:18 -0300

    :refactor: modify app to factors controllers

--- email_service/app.py
+++ email_service/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 
 from email_service import blueprints
+from email_service.blueprints.api import emails
+from email_service.factories.email_controller_factory import email_controller_factory
+from infrastructure.adapters.ses_email_service import SesEmailService
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+
+    emails.init_controller(email_controller_factory(SesEmailService()))
+
     blueprints.init_app(app)
+
     return app
