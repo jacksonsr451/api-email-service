acd5cd663b386e8b7d2868532a29c4277357fb36
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-09-16 10:08:02 -0300

    :feat: implementing infrastructure

---
+++ infrastructure/adapters/ses_email_service.py
@@ -0,0 +1,29 @@
+import boto3
+
+from adapters.email_sender_gateway import EmailSenderGateway
+
+
+class SesEmailService(EmailSenderGateway):
+    def __init__(self) -> None:
+        self.__simple_email_service = boto3.client('ses')
+
+    def send(self, to: str, subject: str, body: str) -> None:
+        self.__simple_email_service.send_email(
+            Source='jacksonsr45@gmail.com',
+            Destination={
+                'ToAddresses': [
+                    to,
+                ]},
+            Message={
+                'Subject': {
+                    'Data': subject,
+                    'Charset': 'utf-8'
+                },
+                'Body': {
+                    'Text': {
+                        'Data': body,
+                        'Charset': 'utf-8'
+                    }
+                }
+            },
+        )
