9172c7421437abc7fd593a9924c35c9a78b679f7
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2023-09-16 10:09:11 -0300

    :feat: create controller

---
+++ controllers/email_controller.py
@@ -0,0 +1,14 @@
+from flask import jsonify
+from application.email_sender_service import EmailSenderService
+
+
+class EmailController:
+    def __init__(self, email_service: EmailSenderService):
+        self.__email_service: EmailSenderService = email_service
+
+    def send(self, to: str, subject: str, body: str):
+        try:
+            self.__email_service.create_email(to, subject, body)
+            return jsonify({'message': 'Email sent'})
+        except Exception as e:
+            return jsonify({'message': str(e)}), 400
